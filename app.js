9fe9fa4babd7d22d17a40cdea533f1993cf51196
Rajaneesh <rajaneesh1998@gmail.com>
2022-09-03 13:00:02 +0530

    Feat: translate btn

---
+++ app.js
@@ -0,0 +1,10 @@
+var btnTranslate = document.querySelector("#btn-translate");
+
+var textInput = document.querySelector("#text-input");
+
+function clickHandler() {
+  console.log("clicked");
+  console.log(textInput.value);
+}
+
+btnTranslate.addEventListener("click", clickHandler);
